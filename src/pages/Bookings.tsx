9e93239e2a09cffc46386ca520b57bd56c90563f
Yakimov1337 <92337248+Yakimov1337@users.noreply.github.com>
2024-09-16 08:28:24 +0300

    Create Bookings.tsx

---
+++ src/pages/Bookings.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import FilteredBookingList from '../components/Booking/FilteredBookingList';
+
+const Bookings: React.FC = () => {
+  return (
+    <FilteredBookingList/>
+  );
+};
+
+export default Bookings;
